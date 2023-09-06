f0f30fee25002879d5c05465ab469002735513a1
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-09-06 13:34:38 +0300

    update

--- lesson-01/example-02/01-variables/02-var-scorping.ts
+++ lesson-01/example-02/01-variables/02-var-scorping.ts
@@ -1,12 +1,32 @@
+var i = 1;
+// console.log(i);         // 1
+
 function var1() {
-  console.log(i);
-  console.log(test);
+  console.log(i);       // undefined
+  console.log(test);    // undefined
   
   for (var i = 0; i < 10; i++) {
     var test = i;    
   };
 
-  console.log(i);
-  console.log(test);
-  
-}
+  console.log(i);       // 10
+  console.log(test);    // 9 
+};
+// var1();
+
+
+function var2() {
+  var a = 10;
+  var a = 20;
+  console.log(a);  
+};
+// var2();                  // 20
+
+function var3() {
+  for (var i = 0; i < 5; i++) {
+    setTimeout(function(){
+      console.log(i);      
+    }, 10)
+  }
+};
+// var3();                     // 5, 5, 5, 5, 5
