e3157f71e0325769b79089fba1c02dd8a5e25d74
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-09-30 22:44:54 +0300

    update

--- lesson-02/02-inheritance/02-base-methods.ts
+++ lesson-02/02-inheritance/02-base-methods.ts
@@ -1,66 +1,71 @@
-// базовий тип даних
+// базовый тип данных
 class Shape {
-    public name: string;
+  // private name: string;
+  public name: string;  
 
-    constructor(name) {
-        this.name = name;
-    }
+  constructor(name) {
+    this.name = name;
+  }
 
-    public getInfo(): string {
-        return `This is ${this.name}.`;
-    }
-}
+  public getInfo(): string {
+    return `This is ${this.name}.`;
+  }
+};
 
-// похідний тип даних
+// наследуемый тип данных
 class Rect extends Shape {
-    private width: number;
-    private height: number;
+  private width: number;
+  private height: number;
 
-    constructor(width: number, height: number) {
-        super('Rectangle'); // запуск конструктора батьківського класу
-        this.width = width;
-        this.height = height;
-    }
+  constructor(width: number, height: number) {
+    // запуск конструктора родительского класса
+    super("Rectangle");
+    this.width = width;
+    this.height = height;
+  }
 
-    public getInfo(): string {
-        const baseInfo: string = super.getInfo();
-        return `${baseInfo} Height = ${this.height}, Width = ${this.width}`;
-    }
-}
+  public getInfo(): string {
+    const baseInfo: string = super.getInfo();
+    return `${baseInfo} Height = ${this.height}, Width = ${this.width}`;
+  }
+};
 
 const newRect: Shape = new Rect(100, 200);
 console.log(newRect.getInfo());
 
-// похідний тип даних
+// наследуемый тип данных
 class Circle extends Shape {
-    private radius: number;
+  private radius: number;
 
-    constructor(radius: number) {
-        super('Circle');
-        this.radius = radius;
-    }
+  constructor(radius: number) {
+    super("Circle");
+    this.radius = radius;
+  }
 
-    public getInfo(): string {
-        const baseInfo: string = super.getInfo(); // виклик методу батьківського класу
-        return `${baseInfo} Radius = ${this.radius}`;
-    }
-}
+  public getInfo(): string {
+    // вызов метода родительского класса
+    const baseInfo: string = super.getInfo();
+    return `${baseInfo} Radius = ${this.radius}`;
+  }
+};
 
-const shapes: Shape[] = []; // створення масиву базового типу
-shapes.push(new Rect(100, 200)); // заповнення масиву екземплярами похідного класу
+// создание массива базового типа
+const shapes: Shape[] = [];
+// заполнение массива экземплярами наследуемого класса
+shapes.push(new Rect(100, 200));
 shapes.push(new Circle(10));
 shapes.push(new Circle(43));
 shapes.push(new Rect(10, 30));
 
-// до масиву shapes можна додавати тільки об'єкти, які мають ідентичну з класом Shape структуру
-shapes.push({
-    name: 'foo',
-    getInfo: () => {
-        return 'bar';
-    },
-});
+// к массиву shapes можно добавить только объекты, содержащие идентичную с классом Shape структуру
+// shapes.push({
+//   name: "foo",
+//   getInfo: () => {
+//     return "bar";
+//   },
+// });
 
 for (let i = 0; i < shapes.length; ++i) {
-    const info: string = shapes[i].getInfo();
-    console.log(info);
+  const info: string = shapes[i].getInfo();
+  console.log(info);
 }
--- lesson-02/02-inheritance/03-abstract.ts
+++ lesson-02/02-inheritance/03-abstract.ts
@@ -1,39 +1,39 @@
-// abstract - ключове слово, яке дозволяє створювати абстрактні методи та абстрактні класи.
-// абстрактний клас - це клас, який може існувати лише в ролі базового класу. Створити екземпляр абстрактного класу не вдасться.
-// абстрактний метод - це метод, який не має реалізації в поточному класі, але обов'язково повинен бути реалізований у похідному класі.
-// абстрактні методи можуть створюватися лише в абстрактних класах.
+// abstract - ключевое слово, позволяющее создавать абстрактные методы и абстрактные классы.
+// Абстрактный класс - это класс, который может существовать только в качестве базового класса. Создать экземпляр абстрактного класса не получится.
+// Абстрактный метод - это метод, не имеющий реализации в текущем классе, но обязательно должен быть реализован в производном классе.
+// Абстрактные методы могут создаваться только в абстрактных классах.
 
-// абстрактний клас
+// абстрактный класс 
 abstract class Animal {
-    constructor(public name: string) {}
+  constructor(public name: string) { }
+  // абстрактный метод 
+  abstract makeSound(): void;
 
-    abstract makeSound(): void; // абстрактний метод
-
-    public move(): void {
-        console.log(this.name + ' пересувається');
-    }
-}
+  public move(): void {
+    console.log(this.name + ' передвигается');
+  }
+};
 
 class Cat extends Animal {
-    constructor() {
-        super('Кіт');
-    }
+  constructor() {
+    super('Кот');
+  }
 
-    // обов'язкова реалізація абстрактного методу із базового класу. Спробуйте видалити цей метод із класу Cat
-    makeSound(): void {
-        console.log('Мяу-Мяу');
-    }
-}
+  // обязательна реализация абстрактного метода из базового класса. Попробуйте удалить этот метод из класса Cat
+  makeSound(): void {
+    console.log('Мяу-Мяу');
+  }
+};
 
 class Cow extends Animal {
-    constructor() {
-        super('Корова');
-    }
+  constructor() {
+    super('Корова');
+  }
 
-    makeSound(): void {
-        console.log('My-y-y-y-y');
-    }
-}
+  makeSound(): void {
+    console.log('My-y-y-y-y');
+  }
+};
 
 const murzik: Animal = new Cat();
 murzik.makeSound();
@@ -43,4 +43,4 @@ const burenka: Animal = new Cow();
 burenka.makeSound();
 burenka.move();
 
-// const animal: Animal = new Animal('animal');
+// const animal: Animal = new Animal('animal');    // Cannot create an instance of an abstract class.
