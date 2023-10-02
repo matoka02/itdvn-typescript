391af411c4baebd783845bba8b5155c3509f2cc6
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-10-02 15:00:00 +0300

    create

---
+++ lesson-04/03-modules/06-import-all.ts
@@ -0,0 +1,4 @@
+import * as validators from "01-validators"; // імпорт всього вмісту модуля в змінну validators
+
+const ccValidtor = new validators.CreditCardValidator();
+const urlValidtor = new validators.UrlValidator();
